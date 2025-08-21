8420bdb22a8eddeb59715a0494a363ea7e2f4cf8
eu-ray29 <raynaravitoria755@gmail.com>
2025-08-21 13:08:52 -0300

    adicionando function manipularSubmitForm

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,27 @@
 import ui from "./ui.js";
+import api from "./api.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    ui.renderizerPensamentos();
+    ui.renderizarPensamentos();
+
+    const formularioPensamento = document.getElementById('pensamento-form');
+    const botaoCancelar = document.getElementById("botao-cancelar")
+
+    formularioPensamento.addEventListener('submit', manipularSubimitForm);
+    botaoCancelar.addEventListener('submit', manipularCancelamento);
+
 });
+
+async function manipularSubimitForm(event) {
+    event.preventDefault();
+    const id = document.getElementById('pensamento-id').value;
+    const conteudo = document.getElementById('pensamento-conteudo').value;
+    const autoria = document.getElementById('pensamento-autoria').value;
+
+    try {
+        await api.salvarPensamento({conteudo, autoria});
+        ui.renderizarPensamentos();
+    } catch {
+        alert('Error ao salvar pensamento');
+    }
+}
